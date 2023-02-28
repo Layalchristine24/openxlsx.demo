b3d443c5bc98809d9cde9cb0776d295d51aeba52
Layal Christine Lettry <layal@cynkra.com>
2023-02-28 07:45:58 +0100

    build: add dependencies to packages and functions

--- R/openxlsx.demo-package.R
+++ R/openxlsx.demo-package.R
@@ -2,9 +2,16 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr arrange
+#' @importFrom dplyr case_when
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr rowwise
+#' @importFrom lubridate today
 #' @importFrom magrittr %>%
 #' @importFrom openxlsx addFilter
 #' @importFrom openxlsx createStyle
 #' @importFrom stringr str_c
+#' @importFrom tidyr crossing
 ## usethis namespace: end
 NULL
