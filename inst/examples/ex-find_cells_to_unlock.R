b19ef9e463b88bde7686c92f50c9d9e96097b4f4
Layal Christine Lettry <layal@cynkra.com>
2023-03-15 14:03:57 +0100

    feat: add example for find_cells_to_unlock

---
+++ inst/examples/ex-find_cells_to_unlock.R
@@ -0,0 +1,10 @@
+#' @examples
+#' \dontrun{
+#' data <- tibble::tribble(
+#'   ~a, ~year, ~f, ~d, ~b, ~c,
+#'   NA, "2019", "g", NA, "f", NA,
+#'   "e", NA, "g", "e", NA, NA,
+#'   NA, "2021", "g", "e", "f", NA,
+#' )
+#' find_cells_to_unlock(data = data, "a", "year", "b", "c")
+#' }
